987dadc4fcc8dc7b82a68a6177f6529904968037
Prasanjeet_Jena <122697153+Pj238icode@users.noreply.github.com>
2025-11-06 01:20:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ import requests
 import boto3
 import os
 
-
+# Environment variables
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 REGION_NAME = os.environ.get("S3_REGION")
 MOVIES_FILE = "movies.pkl"
 SIMILARITY_FILE = "similarity.pkl"
 
-s3 = boto3.client('s3', region_name=REGION_NAME)
+# S3 client with credentials
+s3 = boto3.client(
+    's3',
+    region_name=REGION_NAME,
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY")
+)
 
-
+# Download files if not exist
 def download_from_s3(filename):
     if not os.path.exists(filename):
         s3.download_file(BUCKET_NAME, filename, filename)
 
-# Download files
 download_from_s3(MOVIES_FILE)
 download_from_s3(SIMILARITY_FILE)
 
@@ -25,14 +30,12 @@ download_from_s3(SIMILARITY_FILE)
 movies_df = pickle.load(open(MOVIES_FILE, 'rb'))
 similarity = pickle.load(open(SIMILARITY_FILE, 'rb'))
 
-
-# Function to fetch poster URL
+# Function to fetch poster
 def fetch_poster(movie_name, api_key):
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={movie_name}"
     response = requests.get(url)
     data = response.json()
     return data.get('Poster')
-
 
 # Recommendation function
 def recommend(movie_name, api_key):
@@ -49,18 +52,14 @@ def recommend(movie_name, api_key):
 
     return recommended_movies
 
-
 # Streamlit UI
 st.title("🎬 Movie Recommender System")
-
 movie_name = st.selectbox("Select a movie:", movies_df['title'].values)
 
 if st.button("Recommend"):
     api_key = os.environ.get("OMDB_API_KEY")
     recommended_movies = recommend(movie_name, api_key)
-
     st.success(f"Recommendations for **{movie_name}**:")
-
     cols = st.columns(5)
     for idx, col in enumerate(cols):
         if idx < len(recommended_movies):
@@ -70,6 +69,3 @@ if st.button("Recommend"):
                 col.image(poster_url)
             else:
                 col.write("Poster not available.")
-
-
-
